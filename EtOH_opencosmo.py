29bd3f9c2b66c7eb6d4053cd946b859fb31c0849
ZHChen <769465592@qq.com>
2026-03-17 00:00:49 +0800

    Wrong

--- EtOH_opencosmo.py
+++ EtOH_opencosmo.py
@@ -1,16 +1,16 @@
-from opencosmorspy import CosmoRS  # 导入 openCOSMO-RS 库
+from opencosmorspy.cosmors import COSMORS
 
-# 设定.cosmo 文件路径
-orcacosmo_file = "E:\\Gaussian\\CH3CH2OH\\EtOH.cosmo"
+# cosmo 文件路径
+cosmo_file = r"E:\Gaussian\CH3CH2OH\EtOH.cosmo"
 
-# 创建 CosmoRS 对象
-crs = CosmoRS()
+# 创建 COSMO-RS 对象
+crs = COSMORS()
 
-# 读取 .cosmo 文件
-mol = crs.read_cosmo_file(orcacosmo_file)
+# 读取 cosmo 文件
+mol = crs.read_cosmo_file(cosmo_file)
 
 # 计算 COSMO-RS 描述符
 desc = crs.calculate_descriptors(mol)
 
-# 输出计算结果
-print("Hildebrand 溶解度参数 δ：", desc["delta"])
+# 输出结果
+print("Hildebrand 溶解度参数 δ =", desc["delta"])
